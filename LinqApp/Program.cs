965568aabe095c6b7d3ea75a704c7e75b7002353
KaioMartins <kaio.ccmartins@gmail.com>
2016-11-24 13:32:09 -0200

    GroupBy arquivos do sistema por extensão

--- LinqApp/Program.cs
+++ LinqApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,146 +9,34 @@ namespace LinqApp
 {
     class Program
     {
-        static List<Cliente> CriarClientes()
-        {
-            List<Cliente> clientes = new List<Cliente>();
-
-            clientes.Add(
-                            new Cliente()
-                            {
-                                ID = 1,
-                                Nome = "Felipe Mourato",
-                                Sexo = Cliente.SexoEnum.Masculino,
-                                Nascimento = new DateTime(1980, 12, 1),
-                                Email = "felipe@contoso.com"
-                            }
-                        );
-
-            clientes.Add(
-                            new Cliente()
-                            {
-                                ID = 2,
-                                Nome = "Mauro Mauricio",
-                                Sexo = Cliente.SexoEnum.Masculino,
-                                Nascimento = new DateTime(1970, 6, 12),
-                                Email = "mauro@treinaweb.com.br"
-                            }
-                        );
-
-            clientes.Add(
-                            new Cliente()
-                            {
-                                ID = 3,
-                                Nome = "Suzan Suzi",
-                                Sexo = Cliente.SexoEnum.Feminino,
-                                Nascimento = new DateTime(1992, 2, 25),
-                                Email = "suzan@treinaweb.com.br"
-                            }
-                        );
-
-            clientes.Add(
-                            new Cliente()
-                            {
-                                ID = 4,
-                                Nome = "Claudia Rodrigues",
-                                Sexo = Cliente.SexoEnum.Feminino,
-                                Nascimento = new DateTime(1990, 4, 3),
-                                Email = "claudia@treinaweb.com.br"
-                            }
-                        );
-
-            return clientes;
-        }
-
-        static List<Pedido> CriarPedidos()
-        {
-            List<Pedido> pedidos = new List<Pedido>();
-
-            pedidos.Add(new Pedido()
-            {
-                ID = 1,
-                IdCliente = 1,
-                Data = DateTime.Now,
-                Preco = 40.0
-            });
-            pedidos.Add(new Pedido()
-            {
-                ID = 2,
-                IdCliente = 1,
-                Data = DateTime.Now,
-                Preco = 100.90
-            });
-            pedidos.Add(new Pedido()
-            {
-                ID = 3,
-                IdCliente = 2,
-                Data = DateTime.Now,
-                Preco = 450.0
-            });
-            pedidos.Add(new Pedido()
-            {
-                ID = 4,
-                IdCliente = 3,
-                Data = DateTime.Now,
-                Preco = 32.1
-            });
-            pedidos.Add(new Pedido()
-            {
-                ID = 5,
-                IdCliente = 3,
-                Data = DateTime.Now,
-                Preco = 343.52
-            });
-            pedidos.Add(new Pedido()
-            {
-                ID = 6,
-                IdCliente = 4,
-                Data = DateTime.Now,
-                Preco = 134.98
-            });
-            return pedidos;
-        }
-
-        static void ExibirResultado(List<Cliente> clientes)
-        {
-            foreach (Cliente cliente in clientes)
-            {
-                Console.WriteLine("*******************************");
-                Console.WriteLine("{0} - {1}", cliente.ID, cliente.Nome);
-                Console.WriteLine("Nascimento: {0}", cliente.Nascimento);
-                Console.WriteLine("Sexo: {0}", cliente.Sexo.ToString());
-                Console.WriteLine("Email: {0}", cliente.Email);
-                Console.WriteLine("*******************************");
-                Console.WriteLine("");
-            }
-        }
-
         static void Main(string[] args)
         {
 
-            List<Cliente> clientes = CriarClientes();
-            List<Pedido> pedidos = CriarPedidos();
+            string diretorio = System.Environment.SystemDirectory;
+            Console.WriteLine(diretorio);
 
-            var result = from c in clientes
-                         join p in pedidos
-                         on c.ID equals p.IdCliente
-                         into clientesComPedidos
-                         select new
-                         {
-                             c.Nome,
-                             TotalDePedidos = clientesComPedidos.Count()
-                         };
+            DirectoryInfo dirInfo = new DirectoryInfo(diretorio);
+
+            var arquivos = dirInfo.GetFiles("*.*").ToList();
+
+            var agrupado = from a in arquivos
+                           group a by a.Extension into fgroup
+                           orderby fgroup.Key
+                           select fgroup;
 
             //Em sintaxe de método ficaria da seguinte forma:
-            //var result = clientes.Join(pedidos, c => c.ID, p => p.IdCliente, (cliente, pedido) => new { cliente, pedido }).GroupBy(a => a.cliente.Nome).Select(g => new { Nome = g.Key, TotalDePedidos = g.Count() });
+            //var agrupado = arquivos.GroupBy(a => a.Extension).OrderBy(a => a.Key);
 
-            foreach (var item in result)
+            foreach (var filegroup in agrupado)
             {
-                Console.WriteLine("*******************************");
-                Console.WriteLine(item);
-                Console.WriteLine("*******************************");
-            }
+                Console.WriteLine("{0} - {1} arquivo(s)", filegroup.Key, filegroup.Count());
 
+                foreach (var file in filegroup)
+                {
+                    Console.WriteLine(file.Name);
+                }
+                Console.WriteLine("");
+            }
             Console.ReadKey();
 
         }
